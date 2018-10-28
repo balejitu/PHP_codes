b4886e3eed7357c371c8a78b5d2fec933b2291cc
Jitesh Singla <36637075+balejitu@users.noreply.github.com>
2018-10-28 23:34:07 +0530

    Add files via upload

---
+++ assignment/index.php
@@ -0,0 +1,21 @@
+<title>Jitesh Singla PHP</title>
+<h1>Jitesh Singla Request/Response</h1>
+<p>
+<?php
+echo "The SHA256 hash of \"Jitesh Singla\" is\n";
+print hash('sha256', 'Charles Severance');
+?>
+</p>
+
+
+<pre>
+********
+       *
+       *
+ *     *
+   **  *
+      **
+</pre>
+<a href="fail.php">Click here to check the error setting</a>
+</br>
+<a href="check.php">Click here to cause a traceback<a>
